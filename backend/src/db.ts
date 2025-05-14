546a02dc948ff67a332174bf416e06900797f68c
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2025-05-14 08:27:12 +0600

    successfully category data get code add in backend

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,6 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
-
+import path from "path";
 sqlite3.verbose();
 
 export const getDBConnection = async () => {
@@ -8,4 +8,4 @@ export const getDBConnection = async () => {
         filename: "./dua_main.db",
         driver: sqlite3.Database,
     });
-}
+}   
